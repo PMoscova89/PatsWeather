b576ed753428a2c0f2a42b66bdda683095a04240
Patrick Moscova <pmoscova89@gmail.com>
2026-03-02 18:34:10 -0500

    Initial Commit

---
+++ PatsWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PatsWeather
+//
+//  Created by Patrick Moscova on 3/2/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PatsWeatherTests/PatsWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  PatsWeatherTests.swift
+//  PatsWeatherTests
+//
+//  Created by Patrick Moscova on 3/2/26.
+//
+
+import Testing
+@testable import PatsWeather
+
+struct PatsWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
